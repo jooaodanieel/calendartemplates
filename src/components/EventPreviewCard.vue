a9babdef93386a1826ea5cb0fd73b468e1e23cfb
João Daniel <jotaf.daniel@gmail.com>
2026-03-18 15:03:47 +0100

    add dates to event preview

--- src/components/EventPreviewCard.vue
+++ src/components/EventPreviewCard.vue
@@ -1,12 +1,23 @@
 <template>
   <div :class="['preview-card', { anchor: isAnchor }]">
     <span class="event-label">{{ event.label }}</span>
-    <span class="event-time">{{ event.time }}</span>
+
+    <div class="info-wrapper">
+      <div class="event-info">{{ event.day }}</div>
+      <div v-if="!isSameDay" class="event-info">{{ event.endDay }}</div>
+    </div>
+    
+    <div class="info-wrapper">
+      <div class="event-info">{{ event.time }}</div>
+      <div class="event-info">{{ event.endTime }}</div>
+    </div>
   </div>
 </template>
 
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
   event: {
     type: Object,
     required: true
@@ -16,11 +27,14 @@ defineProps({
     default: false
   }
 })
+
+const isSameDay = computed(() => props.event.day === props.event.endDay)
 </script>
 
 <style scoped>
 .preview-card {
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
   align-items: center;
   padding: 8px 16px;
@@ -28,11 +42,22 @@ defineProps({
   border-radius: 8px;
   width: 100%;
 }
+
 .anchor {
   margin: 12px 0;
+  border: 3px solid #ccc;
+  font-weight: 700;
+  width: calc(100% - 3px);
 }
-.event-time {
+
+.event-info {
   color: #888;
-  font-size: 0.9em;
+  font-size: 0.7em;
+}
+
+.info-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 }
 </style>
